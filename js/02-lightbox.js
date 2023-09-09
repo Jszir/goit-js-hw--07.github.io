7c73f232f4809f9d421540623e08a47946003907
Jszir <123523194+Jszir@users.noreply.github.com>
2023-09-09 10:21:22 -0500

    Colocación de función, quite del inner.html y evento por adjacent

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,28 +1,23 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const galleryContainer = document.querySelector(".gallery");
 
-galleryContainer.innerHTML = galleryItems
-  .map(({ preview, original, description }) => `
-    <li class="gallery__item">
-        <a class="gallery__pic" href="${original}">
-            <img
-            class="gallery__image"
-            data-source="${original}"
-            src="${preview}"
-            alt="${description}"
-            />
-        </a>
-    </li>`
-  )
-  .join("");
-  
-    galleryContainer.style.marginTop =  '100px'
+function createGalleryItem({ preview, original, description }) {
+  return `
+  <li class="gallery__item">
+  <a class="gallery__pic" href="${original}">
+      <img
+      class="gallery__image"
+      data-source="${original}"
+      src="${preview}"
+      alt="${description}"
+      />
+  </a>
+</li>`
+}
 
-
-galleryContainer.addEventListener("click", (e) => {
-  e.preventDefault(); new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
+const galleryMarkup = galleryItems.map(createGalleryItem).join(" ");
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+ new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
 });
-})
